e906f0935849167816b43cd80abf01bfca577a67
DESKTOP-64B8217\kao-tu <patthraporn.tu@mail.kmutt.ac.th>
2019-03-19 10:45:02 +0700

    [ADD] first commit

---
+++ helloworld-laravel/helloworld/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('users', function () {
+    return ('<h1>Users! 5555</h1>');
+});
+Route::get('showname', function () {
+    return ('<h1>Hello Laravel</h1>');
+});
+
